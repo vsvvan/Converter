70184fe0d5bfdbdd00b208a21771df32cb9a7697
vsvvan <valeriia.danina@nure.ua>
2021-05-15 23:51:29 +0300

    Create FormatUtils.java

---
+++ src/main/java/com/sytoss/trainee/utils/FormatUtils.java
@@ -0,0 +1,22 @@
+package com.sytoss.trainee.utils;
+
+public class FormatUtils {
+    private FormatUtils() {
+    }
+
+    public static boolean isCsv(String filename) {
+        StringBuffer fileType = new StringBuffer("");
+        for (int i = filename.length() - 4; i < filename.length(); i++) {
+            fileType.append(filename.charAt(i));
+        }
+        return fileType.toString().equals(".csv");
+    }
+
+    public static boolean isXml(String filename) {
+        StringBuffer fileType = new StringBuffer("");
+        for (int i = filename.length() - 4; i < filename.length(); i++) {
+            fileType.append(filename.charAt(i));
+        }
+        return fileType.toString().equals(".xml");
+    }
+}
